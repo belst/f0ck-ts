5e3f07ea050b6c7f099d665b2dca7bbb858a467d
Joshua Rombauer <joshua.rombauer@tum.de>
2016-08-22 13:33:48 +0200

    adds infinite scroll

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScroll } from 'angular2-infinite-scroll';
 import { Item } from '../item';
 import { F0ckService } from '../f0ck.service';
 
@@ -7,24 +8,37 @@ import '../rxjs-operators';
 @Component({
   selector: 'app-item-list',
   templateUrl: 'item-list.component.html',
-  styleUrls: ['item-list.component.scss']
+  styleUrls: ['item-list.component.scss'],
+  directives: [InfiniteScroll]
 })
 export class ItemListComponent implements OnInit {
   items: Item[] = [];
   errorMessage: string;
+  private page = 1;
 
   constructor(private f0ck: F0ckService) { }
 
   ngOnInit() {
-    this.getItems(1);
+    this.getItems();
   }
 
-  getItems(page = 1) {
-    this.f0ck.getItems(page)
+  getItems() {
+    this.page++;
+    this.f0ck.getItems(this.page - 1)
       .subscribe(
-        items => this.items = items,
+        items => {
+          if(items.length) {
+            this.items = this.items.concat(items);
+          } else {
+            this.page--;
+          }
+        },
         error => this.errorMessage = <any>error
       );
+  }
+
+  onScrollDown() {
+    this.getItems();
   }
 
 }
